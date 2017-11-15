8baeedd59548eafa9525f4029118757a8029f17a
Josh VanderLinden <codekoala@gmail.com>
2017-11-15 01:10:29 -0500

    Initial commit

---
+++ golang.mk
@@ -0,0 +1,18 @@
+APP ?= $(shell basename $(PWD))
+REPO ?= $(shell git config --get remote.origin.url | sed 's,^http.*//,,;s,^.*@,,;s,:,/,;s,\.git$,,')
+TAG ?= $(shell git rev-parse HEAD)-dev
+BUILD_DATE := $(shell date +%FT%T%z)
+BUILD_EXTRA ?= "-o ./bin/$(APP) ./cmd/$(APP)"
+
+# Build the main binary.
+build:
+	@mkdir -p ./bin
+	go build -ldflags '-s -X $(REPO)/version.Commit=$(TAG) -X $(REPO)/version.BuildDate=$(BUILD_DATE)' $(BUILD_EXTRA)
+
+# Run all tests.
+test:
+	@go test -race -cover `go list ./... | grep -v vendor`
+
+# Remove build files.
+clean:
+	rm -rf ./bin
