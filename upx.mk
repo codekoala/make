b0b684efdb31f1f2b46b59dd2fa1fa471ba17030
Josh VanderLinden <codekoala@gmail.com>
2017-11-15 01:34:53 -0500

    Fix tabs

--- upx.mk
+++ upx.mk
@@ -8,8 +8,8 @@ compress: get-upx
 # Fetch a local copy of upx for binary compression (only if it's not already installed)
 get-upx:
 ifeq ($(UPX),)
-        @mkdir -p ./upx/
-        @curl -Ls https://github.com/upx/upx/releases/download/v$(upx_version)/upx-$(upx_version)-amd64_linux.tar.xz | tar Jx -C ./upx/ --strip-components 1
-        $(eval UPX := ./upx/upx)
+	@mkdir -p ./upx/
+	@curl -Ls https://github.com/upx/upx/releases/download/v$(upx_version)/upx-$(upx_version)-amd64_linux.tar.xz | tar Jx -C ./upx/ --strip-components 1
+	$(eval UPX := ./upx/upx)
 endif
-        @$(UPX) --version
+	@$(UPX) --version
