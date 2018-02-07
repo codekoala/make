083c5bde8caf1eba272a48e90513ae746049b2db
Josh VanderLinden <codekoala@gmail.com>
2018-02-07 10:44:13 -0500

    Add docker utilities

---
+++ docker.mk
@@ -0,0 +1,14 @@
+DOCKER_USER ?= $(shell basename `readlink -f ..`)
+DOCKER_REPO ?= $(shell basename `readlink -f .`)
+DOCKER_TAG ?= $(shell git tag)
+DOCKER_IMG ?= $(DOCKER_USER)/$(DOCKER_REPO):$(DOCKER_TAG)
+DOCKER_PATH ?= .
+DOCKER_BUILD_EXTRA ?= --pull
+
+# Build a docker image.
+docker-image:
+	docker build -t $(DOCKER_IMG) $(DOCKER_BUILD_EXTRA) $(DOCKER_PATH)
+
+# Push the docker image to Docker Hub.
+docker-push:
+	docker push $(DOCKER_IMG)
